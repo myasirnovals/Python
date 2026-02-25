daa16488b6daa58cca0968b3283f0ee36d8bdec3
MYN2997 <hawkinsdamien007@gmail.com>
2026-02-26 04:58:51 +0700

    update-terbaru

--- kuliah/DocumentGenerator_Laporan_Praktikum/src/main.py
+++ kuliah/DocumentGenerator_Laporan_Praktikum/src/main.py
@@ -1,10 +1,21 @@
+import os
+import sys
 from ui.app_window import App
 
+# --- Tambahkan fungsi ini di sini ---
+def resource_path(relative_path):
+    """ Dapatkan path absolut ke resource, berfungsi untuk dev dan PyInstaller """
+    try:
+        # PyInstaller membuat folder temp dan menyimpan path di _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 def main():
     app = App()
     app.mainloop()
-
 
 if __name__ == "__main__":
     main()
